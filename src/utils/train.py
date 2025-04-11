96a4c40fbc4ac81473bf20ff0678fcbed1a5ca3c
bolf-marton <bolf-marton@github.com>
2025-04-11 12:32:23 +0200

    added unet codes

--- src/utils/train.py
+++ src/utils/train.py
@@ -10,12 +10,13 @@ from src.utils.comet import CometLogger
 from src.utils.image_tools import get_image
 from src.utils.metrics import calculate_pixel_iou_binary_classification
 
-def maskrcnn_training_testing_loop(model:Any, device:str, train_dataloader:torch.utils.data.DataLoader, optimizer:Any, scheduler:Any, n_epochs:int=0, box_score_thresh:float=0.5, comet_logger:CometLogger=None):
+def maskrcnn_training_testing_loop(model:Any, device:str, train_dataloader:torch.utils.data.DataLoader, test_dataloader:torch.utils.data.DataLoader, optimizer:Any, scheduler:Any, n_epochs:int=0, box_score_thresh:float=0.5, comet_logger:CometLogger=None):
     """Training loop for Mask R-CNN.
     Args:
         model (Any): pytorch NN model.
         device (str): "cuda" / "cpu".
         train_dataloader (torch.utils.data.dataloader.DataLoader): Dataloader for training.
+        test_dataloader (torch.utils.data.dataloader.DataLoader): Dataloader for testing.
         optimizer (Any): the desired optimizer.
         scheduler (Any): the desired scheduler.
         n_epochs (int): number of epochs to train.
@@ -26,7 +27,6 @@ def maskrcnn_training_testing_loop(model:Any, device:str, train_dataloader:torch
         metrics: { 
                 "test_loss" : float,
                 "iou": float,
-                "dice": float
                 }
     """
     if n_epochs != 0:
@@ -52,7 +52,7 @@ def maskrcnn_training_testing_loop(model:Any, device:str, train_dataloader:torch
 
 
                 # TESTING
-                test_metrics = test_maskrcnn(model,device,train_dataloader,optimizer,box_score_thresh)
+                test_metrics = test_maskrcnn(model,device,test_dataloader,optimizer,box_score_thresh)
 
                 # Log metrics to Comet.ml
                 if comet_logger is not None:
@@ -112,7 +112,17 @@ def train_maskrcnn(model, device:str, dataloader: torch.utils.data.DataLoader, o
             'loss_rpn_box_reg': tensor(0.0006, device='cuda:0', grad_fn=<DivBackward0>)}
             }
             """
-            loss = sum(loss for loss in loss_dict.values())
+
+            loss_weights = {
+                'loss_classifier': 1.0,
+                'loss_box_reg': 1.0,
+                'loss_mask': 2.0,  # Emphasizing mask quality
+                'loss_objectness': 1.0,
+                'loss_rpn_box_reg': 1.0
+            }
+
+            # Calculate weighted sum
+            loss = sum(loss_weights[k] * loss_dict[k] for k in loss_dict.keys())
 
             mean_loss += loss
 
@@ -143,7 +153,6 @@ def test_maskrcnn(model:Any, device:str, dataloader:torch.utils.data.DataLoader,
         metrics: { 
                 "test_loss" : float,
                 "iou": float,
-                "dice": float
                 }
     """
 
@@ -212,3 +221,58 @@ def test_maskrcnn(model:Any, device:str, dataloader:torch.utils.data.DataLoader,
     print(f"Test – IoU: {metrics['iou']:.4f}, Loss: {metrics['test_loss']:.4f}")
                     
     return metrics
+
+def unet_training_testing_loop(model:Any, device:str, train_dataloader:torch.utils.data.DataLoader, optimizer:Any, scheduler:Any, n_epochs:int=0, box_score_thresh:float=0.5, comet_logger:CometLogger=None):
+    """Training loop for Mask R-CNN.
+    Args:
+        model (Any): pytorch NN model.
+        device (str): "cuda" / "cpu".
+        train_dataloader (torch.utils.data.dataloader.DataLoader): Dataloader for training.
+        optimizer (Any): the desired optimizer.
+        scheduler (Any): the desired scheduler.
+        n_epochs (int): number of epochs to train.
+        box_score_thresh (float): confidence score threshold for bounding box predictions.
+        comet_logger (CometLogger): Comet.ml logger (optional).
+    Returns:
+        model: trained model.
+        metrics: { 
+                "test_loss" : float,
+                "iou": float,
+                }
+    """
+    if n_epochs != 0:
+            print("\n🚀 STARTING TRAINING")
+
+            for epoch in range(n_epochs):
+                if comet_logger is not None:
+                    print("-" * len(f"Epoch {epoch+1}/{n_epochs} - {comet_logger.name}"))
+                    print(f"Epoch {epoch+1}/{n_epochs} - {comet_logger.name}")
+                    print("-" * len(f"Epoch {epoch+1}/{n_epochs} - {comet_logger.name}"))
+                else:
+                    print("-" * len(f"Epoch {epoch+1}/{n_epochs}"))
+                    print(f"Epoch {epoch+1}/{n_epochs}")
+                    print("-" * len(f"Epoch {epoch+1}/{n_epochs}"))
+
+                # TRAINING
+                model, train_metrics = train_maskrcnn(model,device,train_dataloader,optimizer)
+                
+                # Log metrics to Comet.ml
+                if comet_logger is not None:
+                    for k in train_metrics.keys():
+                        comet_logger.log_metric(k, train_metrics[k], epoch=epoch + 1)
+
+
+                # TESTING
+                test_metrics = test_maskrcnn(model,device,train_dataloader,optimizer,box_score_thresh)
+
+                # Log metrics to Comet.ml
+                if comet_logger is not None:
+                    for k in test_metrics.keys():
+                        comet_logger.log_metric(k, test_metrics[k], epoch=epoch + 1)
+
+                # SCHEDULER
+                if scheduler is not None:
+                    scheduler.step(test_metrics["iou"])
+                # Log learning rate to Comet.ml
+                if comet_logger is not None:
+                    comet_logger.log_metric("learning_rate", scheduler.get_last_lr(), epoch=epoch + 1)
